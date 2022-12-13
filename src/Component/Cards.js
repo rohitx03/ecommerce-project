6b80427b92cce64a19b3da2eaa90f5c474e87bba
Rohit <rohit.sagar.x03@gmail.com>
2022-12-13 12:09:56 +0530

    done

--- src/Component/Cards.js
+++ src/Component/Cards.js
@@ -46,7 +46,7 @@ const Cards=() =>{
                 <div class="card">
                   <img src="..." class="card-img-top" alt="..." />
                     <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
+                      <h5 class="card-title">Cddddddddddddard title</h5>
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                       <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
